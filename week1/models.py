a6267e4f9080ab6270ef45f6b47841b7bb5ec4a1
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-08-21 21:10:11 +0000

    renamed
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@16121 1f418930-7ff8-0310-b8ad-c653122473bc

--- week1/models.py
+++ week1/models.py
@@ -4,17 +4,17 @@ import datetime
 # Create your models here.
 from game.installed_games import InstalledGames,GameInterface
 
-class Turn1(GameInterface):
+class Week1(GameInterface):
     def pages(self):
         return ('index',)
 
     def template(self,page_id=None):
         game_context = {'sampledata':"hello", 'duedate':datetime.datetime.today()}
-        return ('turn1/index.html',game_context)
+        return ('week1/index.html',game_context)
     
     def variables(self,page_id=None):
         return []
 
-InstalledGames.register_game('turn1',
-                             'Turn 1',
-                             Turn1() )
+InstalledGames.register_game('week1',
+                             'Week 1',
+                             Week1() )

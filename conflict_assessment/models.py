fbdcd4cbdf32e8fbb5d3bb4f6dab5c920957a7dd
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-09-09 23:20:23 +0000

    added document using page_id
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@16509 1f418930-7ff8-0310-b8ad-c653122473bc

--- conflict_assessment/models.py
+++ conflict_assessment/models.py
@@ -9,6 +9,8 @@ class ConflictAssessment(GameInterface):
 
     def template(self,page_id=None):
         game_context = {'sampledata':"hello"}
+        if page_id == "country_narrative":
+          return ('conflict_assessment/narrative.html',game_context)
         return ('conflict_assessment/index.html',game_context)
     
     def variables(self,page_id=None):

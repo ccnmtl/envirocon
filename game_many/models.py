e87a3180dfed64f72a58c1c113d9e4942804f4d1
jonah <jonah@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-09-16 16:10:27 +0000

    fill out skeleton of remaining activities
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@16678 1f418930-7ff8-0310-b8ad-c653122473bc

--- game_many/models.py
+++ game_many/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from game.installed_games import InstalledGames,GameInterface
 
 class ExplainYourReportSelection(GameInterface):
+    """
+    Week 2, Recommending Interventions
+    """
+
     def pages(self):
         return ('index',)
 
@@ -20,6 +24,9 @@ InstalledGames.register_game('explain_your_report_selection',
 
 
 class RecommendingInterventions(GameInterface):
+    """
+    Week 3, Recommending Interventions
+    """
     def pages(self):
         return ('index',)
 
@@ -33,3 +40,94 @@ class RecommendingInterventions(GameInterface):
 InstalledGames.register_game('recommending_interventions',
                              'Recommending Interventions',
                              RecommendingInterventions() )
+
+class FundingInterventions(GameInterface):
+    """
+    Week 4, Funding Interventions
+    """
+    def pages(self):
+        return ('index',)
+
+    def template(self,page_id=None):
+        game_context = {'sampledata':"hello"}
+        return ('game_many/funding_interventions.html',game_context)
+    
+    def variables(self,page_id=None):
+        return ['funding_interventions']
+
+InstalledGames.register_game('funding_interventions',
+                             'Funding Interventions',
+                             FundingInterventions() )
+
+class TrackingYourProjects(GameInterface):
+    """
+    Week 4, Tracking Your Projects
+    """
+    def pages(self):
+        return ('index',)
+
+    def template(self,page_id=None):
+        game_context = {'sampledata':"hello"}
+        return ('game_many/tracking_your_projects.html',game_context)
+    
+    def variables(self,page_id=None):
+        return ['tracking_your_projects']
+
+InstalledGames.register_game('tracking_your_projecst',
+                             'Tracking Your Projects',
+                             TrackingYourProjects() )
+
+class ResultsFramework(GameInterface):
+    """
+    Week 5, Results Framework
+    """
+    def pages(self):
+        return ('index',)
+
+    def template(self,page_id=None):
+        game_context = {'sampledata':"hello"}
+        return ('game_many/results_framework.html',game_context)
+    
+    def variables(self,page_id=None):
+        return ['results_framework']
+
+InstalledGames.register_game('results_framework',
+                             'Results Framework',
+                             ResultsFramework() )
+
+class DonorsConference(GameInterface):
+    """
+    Week 6, Donors Conference & More Interventions
+    """
+    def pages(self):
+        return ('index',)
+
+    def template(self,page_id=None):
+        game_context = {'sampledata':"hello"}
+        return ('game_many/donors_conference.html',game_context)
+    
+    def variables(self,page_id=None):
+        return ['donors_conference']
+
+InstalledGames.register_game('donors_conference',
+                             'Donors Conference',
+                             DonorsConference() )
+
+class FinalPaper(GameInterface):
+    """
+    Week 8, Final Paper
+    """
+    def pages(self):
+        return ('index',)
+
+    def template(self,page_id=None):
+        game_context = {'sampledata':"hello"}
+        return ('game_many/final_paper.html',game_context)
+    
+    def variables(self,page_id=None):
+        return ['final_paper']
+
+InstalledGames.register_game('final_paper',
+                             'Final Paper',
+                             FinalPaper() )
+

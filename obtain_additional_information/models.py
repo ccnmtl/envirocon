77a440d050bc5b8dcdd24a872b6815c390501c5e
khagan <khagan@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-09-17 15:58:10 +0000

    added intro page for obtain additional information activity
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@16716 1f418930-7ff8-0310-b8ad-c653122473bc

--- obtain_additional_information/models.py
+++ obtain_additional_information/models.py
@@ -5,11 +5,14 @@ from game.installed_games import InstalledGames,GameInterface
 
 class ObtainAdditionalInformation(GameInterface):
     def pages(self):
-        return ('index',)
+        return ('index','page2')
 
     def template(self,page_id=None,public_state=None):
         game_context = {'sampledata':"hello"}
-        return ('obtain_additional_information/index.html',game_context)
+        if page_id == "page2":
+          return ('obtain_additional_information/index.html',game_context)
+        return ('obtain_additional_information/intro.html',game_context)
+        
     
     def variables(self,page_id=None):
         return ['additional_information']

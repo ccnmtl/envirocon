45613e580f8debed43125bcb19652f24e860d703
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2009-11-10 19:44:31 +0000

    ok, this fixes the mysterious kodos reverse() problem
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@17914 1f418930-7ff8-0310-b8ad-c653122473bc

--- urls.py
+++ urls.py
@@ -4,22 +4,29 @@ from django.conf import settings
 import os.path
 admin.autodiscover()
 
+import survey.urls
+import djangowind.urls
+#import tinymce.urls
+import game.urls
+import statefulgame.urls
+import teams.urls
+
 site_media_root = os.path.join(os.path.dirname(__file__),"media")
 
 urlpatterns = patterns('',
                        # Example:
                        # (r'^envirocon/', include('envirocon.foo.urls')),
                        (r'^logout$', 'django.contrib.auth.views.logout', {'next_page':'/accounts/login/?next=/'}),
-                       ('^accounts/',include('djangowind.urls')),
+                       ('^accounts/',include(djangowind.urls)),
                        (r'^admin/(.*)', admin.site.root),
-		       (r'^survey/',include('survey.urls')),
-                       (r'^tinymce/', include('tinymce.urls')),
+		       (r'^survey/',include(survey.urls)),
+#                       (r'^tinymce/', include(tinymce.urls)),
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': site_media_root}),
                        (r'^uploads/(?P<path>.*)$','django.views.static.serve',{'document_root' : settings.MEDIA_ROOT}),
 
-                       (r'^gamepages/',include('game.urls')),
-                       (r'',include('statefulgame.urls')), #import at root
-                       (r'^teams/',include('teams.urls')),
+                       (r'^gamepages/',include(game.urls)),
+                       (r'',include(statefulgame.urls)), #import at root
+                       (r'^teams/',include(teams.urls)),
 
                        (r'^about','envirocon_controller.views.about'),
                        (r'^help','envirocon_controller.views.help'),

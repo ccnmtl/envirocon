a8945edae4427a3cfc0a50db9aadbb451eddc9e3
sky <sky@1f418930-7ff8-0310-b8ad-c653122473bc>
2010-07-14 14:03:49 +0000

    really closes: #66338 add expand/collapse arrow
    git-svn-id: http://svn.ccnmtl.columbia.edu/envirocon/trunk@19141 1f418930-7ff8-0310-b8ad-c653122473bc

--- media/js/toggle_full_info.js
+++ media/js/toggle_full_info.js
@@ -1,15 +1,3 @@
-function toggleInfo(e) {
-  var parent = getFirstParentByTagAndClassName(e.src(), "div");
-  var child = getFirstElementByTagAndClassName("*", "toggle", parent);
-  var visible = getStyle(child, "display");
-  if(visible == "block") {
-      toggleClose(child, e.src());
-  }
-  else {
-      toggleOpen(child, e.src());
-  }
-}
-
 function initToggleInfo() {
   forEach(getElementsByTagAndClassName("*", "toggle-control"), function(elem) {
     connect(elem, "onclick", toggleInfo);
@@ -20,20 +8,33 @@ function initToggleInfo() {
     }
 }
 
-function toggleOpen(child, widget) {
-    widget = widget || getFirstElementByTagAndClassName(
-        "*", "toggle-control",
-        getFirstParentByTagAndClassName(child, "div"));
-    setStyle(child, {'display':'block'});
-    setStyle(widget, {'background-image':'url(/site_media/img/toggle_open.gif)'});
+function toggleInfo(e) {
+    var parent = getFirstParentByTagAndClassName(e.src(), "div");
+    var visible = /toggle_open/.test(getStyle(e.src(),'backgroundImage'));
+    if(visible) {
+        toggleClose(parent, e.src());
+    }
+    else {
+        toggleOpen(parent, e.src());
+    }
+}
+function toggleOpen(parent, widget) {
+    forEach(getElementsByTagAndClassName('*','toggle',parent), function(child) {
+        setStyle(child, {'display':'block'});        
+    });
+    forEach(getElementsByTagAndClassName('*','toggle-control',parent), function(w){
+        setStyle(w, {'background-image':'url(/site_media/img/toggle_open.gif)'});
+    });
 }
 
-function toggleClose(child, widget) {
-    widget = widget || getFirstElementByTagAndClassName(
-        "*", "toggle-control",
-        getFirstParentByTagAndClassName(child, "div"));
-    setStyle(child, {'display':'none'});
-    setStyle(widget, {'background-image':'url(/site_media/img/toggle_closed.gif)'});
+function toggleClose(parent, widget) {
+    //widget = widget || getFirstElementByTagAndClassName("*", "toggle-control",parent);
+    forEach(getElementsByTagAndClassName('*','toggle',parent), function(child) {
+        setStyle(child, {'display':'none'});        
+    });
+    forEach(getElementsByTagAndClassName('*','toggle-control',parent), function(w){
+        setStyle(w, {'background-image':'url(/site_media/img/toggle_closed.gif)'});
+    });
 }
 
 function toggleAll(evt) {
